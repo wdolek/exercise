fd5e4bebe1fd82459e9f50d0dc1b0301e7bf9fb0
Zdenek Havlin <wdolek@hotmail.com>
2019-11-13 10:42:12 +0100

    Add solution of '949. Largest Time for Given Digits'

---
+++ L33tC0d3/LC.Tests/LargestTimeForDigits/TimeDigitatorShould.cs
@@ -0,0 +1,32 @@
+﻿using LC.LargestTimeForDigits;
+using Xunit;
+
+namespace LC.Tests.LargestTimeForDigits
+{
+    public class TimeDigitatorShould
+    {
+        [Theory]
+        [InlineData(new [] { 1, 2, 3, 4 }, "23:41")]
+        [InlineData(new [] { 2, 2, 2, 2 }, "22:22")]
+        [InlineData(new [] { 9, 5, 0, 1 }, "19:50")]
+        public void ReturnMaxTime(int[] input, string expected)
+        {
+            string result = new TimeDigitator().LargestTimeFromDigits(input);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Theory]
+        [InlineData(new [] { 3, 4, 5, 6 })]
+        [InlineData(new [] { 6, 7, 8, 9 })]
+        [InlineData(new [] { 0 })]
+        [InlineData(new [] { 0, 1, 2, 3, 4 })]
+        [InlineData(null)]
+        public void ReturnEmpty(int[] input)
+        {
+            string result = new TimeDigitator().LargestTimeFromDigits(input);
+
+            Assert.True(string.IsNullOrEmpty(result));
+        }
+    }
+}
---
+++ L33tC0d3/LC/LargestTimeForDigits/TimeDigitator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LC.LargestTimeForDigits
+{
+    // https://leetcode.com/problems/largest-time-for-given-digits/submissions/
+    public sealed class TimeDigitator
+    {
+        public string LargestTimeFromDigits(int[] A)
+        {
+            if (A == null || A.Length != 4)
+            {
+                return string.Empty;
+            }
+
+            // no possible hour, H*:**
+            if (A.All(i => i > 2))
+            {
+                return string.Empty;
+            }
+
+            // no possible minute, **:M*
+            if (A.All(i => i > 5))
+            {
+                return string.Empty;
+            }
+
+            var times = new List<(int Hours, int Minutes)>();
+
+            // generate all valid combinations
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    for (var k = 0; k < 4; k++)
+                    {
+                        if (k == j || k == i)
+                        {
+                            continue;
+                        }
+
+                        // we can calculate `l` right away:
+                        // - 6 possible combinations
+                        // - must not equal to any of `i`, `j` or `k`
+                        //
+                        // for (var l = 0; l < 4; l++) {
+                        //     if (l == k || l == j || l == i) continue;
+                        //     ...
+                        // }
+                        int l = 6 - i - j - k;
+
+                        int hours = A[i] * 10 + A[j];
+                        int minutes = A[k] * 10 + A[l];
+                        if (hours < 24 && minutes < 60)
+                        {
+                            times.Add((hours, minutes));
+                        }
+                    }
+                }
+            }
+
+            if (times.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            // find the largest time
+            (int h, int m) = times
+                .OrderByDescending(t => t.Hours * 60 + t.Minutes)
+                .First();
+
+            return $"{h:D2}:{m:D2}";
+        }
+    }
+}

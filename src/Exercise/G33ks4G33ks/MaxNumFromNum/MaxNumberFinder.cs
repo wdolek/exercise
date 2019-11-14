cfe51ca52121602dd5dcab25d8281bfbc2a6e26e
Zdenek Havlin <wdolek@hotmail.com>
2019-11-14 23:31:13 +0100

    Add Geeks For Geeks namespace and first problem/task

---
+++ src/Exercise/G33ks4G33ks/MaxNumFromNum/MaxNumberFinder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise.G33ks4G33ks.MaxNumFromNum
+{
+    // https://www.geeksforgeeks.org/find-maximum-number-can-formed-digits-number-reviewed/
+    public class MaxNumberFinder
+    {
+        public int PrintMaxNum(int num)
+        {
+            // collect each digit from number
+            var digits = new List<int>();
+            var value = num;
+            while (value > 0)
+            {
+                digits.Add(value % 10);
+                value /= 10;
+            }
+
+            // sort digits from lowest to highest
+            // NB! tis uses List<T>.Sort, not a LINQ one
+            digits.Sort();
+
+            // calculate the highest possible integer
+            // - start with highest digits
+            var result = 0;
+            for (var i = digits.Count - 1; i >= 0; i--)
+            {
+                result = result * 10 + digits[i];
+            }
+
+            return result;
+        }
+
+        public int PrintMaxNumWithString(int num)
+        {
+            // GeeksForGeeks solution
+            int[] count = new int[10];
+            var str = num.ToString();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                count[str[i] - '0']++;
+            }
+
+            int result = 0;
+            var multiplier = 1;
+
+            for (int i = 0; i <= 9; i++)
+            {
+                while (count[i] > 0)
+                {
+                    result += i * multiplier;
+                    count[i]--;
+                    multiplier *= 10;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/Exercise.Tests/G33ks4G33ks/MaxNumFromNum/MaxNumberFinderShould.cs
@@ -0,0 +1,35 @@
+﻿using Exercise.G33ks4G33ks.MaxNumFromNum;
+using Xunit;
+
+namespace Exercise.Tests.G33ks4G33ks.MaxNumFromNum
+{
+    public class MaxNumberFinderShould
+    {
+        [Theory]
+        [MemberData(nameof(GenerateNumbers))]
+        public void FindMaxPossibleNumber(int input, int expected)
+        {
+            var finder = new MaxNumberFinder();
+
+            Assert.Equal(expected, finder.PrintMaxNum(input));
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateNumbers))]
+        public void FindMaxPossibleNumberUsingStr(int input, int expected)
+        {
+            var finder = new MaxNumberFinder();
+
+            Assert.Equal(expected, finder.PrintMaxNumWithString(input));
+        }
+
+        public static TheoryData<int, int> GenerateNumbers() =>
+            new TheoryData<int, int>
+            {
+                { 123, 321 },
+                { 8754365, 8765543 },
+                { 38293367, 98763332 },
+                { 1203465, 6543210 }
+            };
+    }
+}

13ca769e3c4e9278d677450e1c6c299c44114c93
Zdenek Havlin <wdolek@hotmail.com>
2019-11-15 21:42:41 +0100

    Add Regex matcher (... not solved myself :( - solution taken from LeetCode)

---
+++ src/Exercise/L33tC0d3/RegexMatcher/Regexator.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise.L33tC0d3.RegexMatcher
+{
+    // https://leetcode.com/problems/regular-expression-matching/
+    public class Regexator
+    {
+        public bool IsMatch(string s, string p)
+        {
+            // dynamic programming (https://en.wikipedia.org/wiki/Dynamic_programming)
+            // (implementation taken from LeetCode)
+            bool[,] dp = new bool[s.Length + 1, p.Length + 1];
+            dp[s.Length, p.Length] = true;
+
+            for (int i = s.Length; i >= 0; i--)
+            {
+                for (int j = p.Length - 1; j >= 0; j--)
+                {
+                    var firstMatch = (i < s.Length && (p[j] == s[i] || p[j] == '.'));
+
+                    if (j + 1 < p.Length && p[j + 1] == '*')
+                    {
+                        dp[i,j] = dp[i, j + 2] || firstMatch && dp[i + 1, j];
+                    }
+                    else
+                    {
+                        dp[i, j] = firstMatch && dp[i + 1, j + 1];
+                    }
+                }
+            }
+
+            return dp[0, 0];
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/RegexMatcher/RegexatorShould.cs
@@ -0,0 +1,22 @@
+﻿using Exercise.L33tC0d3.RegexMatcher;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.RegexMatcher
+{
+    public class RegexatorShould
+    {
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "a.", true)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("ab", ".*", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("mississippi", "mis*is*p*.", false)]
+        public void DetermineWhetherInputMatchesPattern(string input, string pattern, bool expected)
+        {
+            var regexator = new Regexator();
+
+            Assert.Equal(expected, regexator.IsMatch(input, pattern));
+        }
+    }
+}

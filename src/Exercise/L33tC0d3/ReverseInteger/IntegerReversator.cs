9b19b741162c03490aec819f7a46ea4f774fbbfb
Zdenek Havlin <wdolek@hotmail.com>
2019-11-15 20:33:38 +0100

    Add Reverse integer

---
+++ src/Exercise/L33tC0d3/ReverseInteger/IntegerReversator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise.L33tC0d3.ReverseInteger
+{
+    // https://leetcode.com/problems/reverse-integer/
+    public sealed class IntegerReversator
+    {
+        public int Reverse(int x)
+        {
+            var result = 0;
+            try
+            {
+                while (x != 0)
+                {
+                    result = checked((result * 10) + (x % 10));
+                    x /= 10;
+                }
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/ReverseInteger/IntegerReversatorShould.cs
@@ -0,0 +1,20 @@
+﻿using Exercise.L33tC0d3.ReverseInteger;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.ReverseInteger
+{
+    public class IntegerReversatorShould
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(1534236469, 0)]
+        public void ReverseNumberCorrectly(int input, int expected)
+        {
+            var reversator = new IntegerReversator();
+
+            Assert.Equal(expected, reversator.Reverse(input));
+        }
+    }
+}

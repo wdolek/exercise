89f6ed6295c0ae464ca41de7b9e88d5ba236d369
Zdenek Havlin <wdolek@hotmail.com>
2019-11-15 21:04:21 +0100

    Add Number palindrome

---
+++ src/Exercise/L33tC0d3/PalindromeNumber/NumberPalindrator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise.L33tC0d3.PalindromeNumber
+{
+    // https://leetcode.com/problems/palindrome-number/
+    public sealed class NumberPalindrator
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x != 0 && x % 10 == 0))
+            {
+                return false;
+            }
+
+            // split to digits
+            var digits = new List<int>();
+            while (x > 0)
+            {
+                digits.Add(x % 10);
+                x /= 10;
+            }
+
+            // check first half of list with second half
+            for (var i = 0; i < digits.Count / 2; i++)
+            {
+                if (digits[i] != digits[digits.Count - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsPalindromeSolution(int x)
+        {
+            // taken from submitted answers
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            var result = 0;
+            while (x > result)
+            {
+                result = (result * 10) + (x % 10);
+                x /= 10;
+            }
+
+            return x == result || x == result / 10;
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/PalindromeNumber/NumberPalindratorShould.cs
@@ -0,0 +1,23 @@
+﻿using Exercise.L33tC0d3.PalindromeNumber;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.PalindromeNumber
+{
+    public class NumberPalindratorShould
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(123321, true)]
+        [InlineData(123456, false)]
+        [InlineData(102010, false)]
+        [InlineData(0, true)]
+        public void DetectNumberPalindrome(int input, bool expected)
+        {
+            var palindrator = new NumberPalindrator();
+
+            Assert.Equal(expected, palindrator.IsPalindrome(input));
+        }
+    }
+}

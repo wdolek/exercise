5cedc85471d0f275aa3672faf958421a32685987
Zdenek Havlin <wdolek@hotmail.com>
2019-11-15 15:18:16 +0100

    Add `TwoNumberAdder`

---
+++ src/Exercise/L33tC0d3/AddTwoNumbers/TwoNumberAdder.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace Exercise.L33tC0d3.AddTwoNumbers
+{
+    // https://leetcode.com/problems/add-two-numbers/
+    public sealed class TwoNumberAdder
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            //   [2 -> 4 -> 3]
+            // + [5 -> 6 -> 4]
+            // ---------------
+            //   [7 -> 0 -> 8]
+            //
+            // (342 + 465 = 807)
+
+            var temp1 = l1;
+            var temp2 = l2;
+
+            var resultRoot = new ListNode(-1);
+            var tempResult = resultRoot;
+
+            var remainder = 0;
+
+            while (temp1 != null || temp2 != null)
+            {
+                var sum = (temp1?.val).GetValueOrDefault()
+                    + (temp2?.val).GetValueOrDefault()
+                    + remainder;
+
+                remainder = sum >= 10
+                    ? 1
+                    : 0;
+
+                // store result
+                tempResult.next = new ListNode(sum - (remainder * 10));
+                tempResult = tempResult.next;
+
+                // move to next if exists
+                temp1 = temp1?.next;
+                temp2 = temp2?.next;
+            }
+
+            // apply last remainder
+            if (remainder > 0)
+            {
+                tempResult.next = new ListNode(remainder);
+            }
+
+            // strip first node
+            return resultRoot.next;
+        }
+
+        [DebuggerDisplay("{val}")]
+        public sealed class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/AddTwoNumbers/TwoNumberAdderShould.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Exercise.L33tC0d3.AddTwoNumbers;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.AddTwoNumbers
+{
+    public class TwoNumberAdderShould
+    {
+        [Theory]
+        [MemberData(nameof(GenerateData))]
+        public void CalculateCorrectNumber(TwoNumberAdder.ListNode x, TwoNumberAdder.ListNode y, int expected)
+        {
+            var adder = new TwoNumberAdder();
+            var result = adder.AddTwoNumbers(x, y);
+
+            Assert.Equal(expected, ToInt(result));
+        }
+
+        public static TheoryData<TwoNumberAdder.ListNode, TwoNumberAdder.ListNode, int> GenerateData()
+        {
+            return new TheoryData<TwoNumberAdder.ListNode, TwoNumberAdder.ListNode, int>
+            {
+                // 342 + 465 = 807
+                {
+                    new TwoNumberAdder.ListNode(2).Add(4).Add(3),
+                    new TwoNumberAdder.ListNode(5).Add(6).Add(4),
+                    807
+                },
+                // 81 + 0 = 81
+                {
+                    new TwoNumberAdder.ListNode(1).Add(8),
+                    new TwoNumberAdder.ListNode(0),
+                    81
+                }
+            };
+        }
+
+        private static IEnumerable<int> Unwrap(TwoNumberAdder.ListNode node)
+        {
+            var tmp = node;
+            while (tmp != null)
+            {
+                yield return tmp.val;
+                tmp = tmp.next;
+            }
+        }
+
+        private static int ToInt(TwoNumberAdder.ListNode node)
+        {
+            var nums = Unwrap(node).ToArray();
+            var result = 0;
+            for (var i = nums.Length - 1; i >= 0; i--)
+            {
+                result = nums[i] + result * 10;
+            }
+
+            return result;
+        }
+    }
+
+    internal static class ListNodeExtensions
+    {
+        public static TwoNumberAdder.ListNode Add(this TwoNumberAdder.ListNode node, int i)
+        {
+            // iterate to last node
+            var temp = node;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = new TwoNumberAdder.ListNode(i);
+
+            return node;
+        }
+    }
+}

72603876bbc0cf92cc0662227d4134aba3b5ea44
Zdenek Havlin <wdolek@hotmail.com>
2021-02-05 22:30:54 +0100

    Add romano-integerator

---
+++ src/Exercise/L33tC0d3/RomanToInteger/RomanoIntegerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise.L33tC0d3.RomanToInteger
+{
+    // https://leetcode.com/problems/roman-to-integer/
+    public class RomanoIntegerator
+    {
+        private static readonly Dictionary<char, ushort> Literals =
+            new Dictionary<char, ushort>
+            {
+                ['M'] = 1000,
+                ['D'] = 500,
+                ['C'] = 100,
+                ['L'] = 50,
+                ['X'] = 10,
+                ['V'] = 5,
+                ['I'] = 1,
+            };
+
+        public int RomanToInt(string s)
+        {
+            var result = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var current = Literals[s[i]];
+                var next = i < s.Length - 1
+                    ? Literals[s[i + 1]]
+                    : 0;
+
+                if (current >= next)
+                {
+                    result += current;
+                }
+                else
+                {
+                    result += (next - current);
+                    i += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/RomanToInteger/RomanoIntegeratorShould.cs
@@ -0,0 +1,20 @@
+﻿using Exercise.L33tC0d3.RomanToInteger;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.RomanToInteger
+{
+    public class RomanoIntegeratorShould
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void ReturnValidInteger(string roman, int expected)
+        {
+            var integerator = new RomanoIntegerator();
+            Assert.Equal(expected, integerator.RomanToInt(roman));
+        }
+    }
+}

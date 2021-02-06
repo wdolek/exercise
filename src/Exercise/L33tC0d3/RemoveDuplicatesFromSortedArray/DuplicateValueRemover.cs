7bdf219d25374a2323a37fa9897c57398bea79eb
Zdenek Havlin <wdolek@hotmail.com>
2021-02-06 23:40:43 +0100

    Add duplicate value remover

---
+++ src/Exercise/L33tC0d3/RemoveDuplicatesFromSortedArray/DuplicateValueRemover.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise.L33tC0d3.RemoveDuplicatesFromSortedArray
+{
+    public class DuplicateValueRemover
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            // start with 1: first value is unique for sure :)
+            int numOfUniqueValues = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                // if {previous} != {current} ...
+                if (nums[numOfUniqueValues - 1] != nums[i])
+                {
+                    // move {current} to {previous} ...
+                    // (moves current value to last known position of duplicate)
+                    nums[numOfUniqueValues] = nums[i];
+
+                    // count +1 for unique number
+                    ++numOfUniqueValues;
+                }
+            }
+
+            return numOfUniqueValues;
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/RemoveDuplicatesFromSortedArray/DuplicateValueRemoverShould.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Exercise.L33tC0d3.RemoveDuplicatesFromSortedArray;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.RemoveDuplicatesFromSortedArray
+{
+    public class DuplicateValueRemoverShould
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 2 })]
+        [InlineData(new[] { 1, 1, 2, 2 })]
+        [InlineData(new[] { 1, 1, 1, 2, 2, 2 })]
+        [InlineData(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 })]
+        public void DetectDuplicatesCorrectly(int[] nums)
+        {
+            var uniqueValues = new HashSet<int>(nums);
+
+            var remover = new DuplicateValueRemover();
+            var numOfUniqueValues = remover.RemoveDuplicates(nums);
+
+            Assert.Equal(uniqueValues.Count, numOfUniqueValues);
+            Assert.Equal(uniqueValues, nums.Take(numOfUniqueValues));
+        }
+    }
+}

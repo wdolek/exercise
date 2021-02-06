8d8888e4a0a5d85112e81c14353bc3f6190e713b
Zdenek Havlin <wdolek@hotmail.com>
2021-02-06 20:54:10 +0100

    Add parenthesis validator

---
+++ src/Exercise/L33tC0d3/ValidParentheses/ParenthesesValidator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise.L33tC0d3.ValidParentheses
+{
+    // https://leetcode.com/problems/valid-parentheses/
+    public class ParenthesesValidator
+    {
+        public bool IsValid(string s)
+        {
+            var brackets = new Stack<char>();
+            foreach (var bracket in s)
+            {
+                if (IsOpening(bracket))
+                {
+                    brackets.Push(bracket);
+                    continue;
+                }
+
+                if (brackets.Count == 0)
+                {
+                    return false;
+                }
+
+                var last = brackets.Pop();
+                if (!IsPair(last, bracket))
+                {
+                    return false;
+                }
+            }
+
+            return brackets.Count == 0;
+        }
+
+        private bool IsOpening(char symbol)
+        {
+            return symbol == '('
+                || symbol == '{'
+                || symbol == '[';
+        }
+
+        private bool IsPair(char left, char right)
+        {
+            return (left == '(' && right == ')')
+                || (left == '{' && right == '}')
+                || (left == '[' && right == ']');
+        }
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/ValidParentheses/ParenthesesValidatorShould.cs
@@ -0,0 +1,21 @@
+﻿using Exercise.L33tC0d3.ValidParentheses;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.ValidParentheses
+{
+    public class ParenthesesValidatorShould
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("([)]", false)]
+        [InlineData("{[]}", true)]
+        public void ValidateParenthesesInGivenString(string s, bool expected)
+        {
+            var validator = new ParenthesesValidator();
+
+            Assert.Equal(expected, validator.IsValid(s));
+        }
+    }
+}

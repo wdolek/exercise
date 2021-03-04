e35155e09e580be6ae618e19ca5f45b075890c3d
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:33:44 +0100

    Drawing Book

---
+++ src/Exercise/H4x0rR4nk/DrawingBook/PageCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise.H4x0rR4nk.DrawingBook
+{
+    // https://www.hackerrank.com/challenges/drawing-book/problem
+    public class PageCounter
+    {
+        static int pageCount(int n, int p)
+        {
+            return Math.Min(
+                p / 2,
+                n / 2 - p / 2);
+        }
+    }
+}

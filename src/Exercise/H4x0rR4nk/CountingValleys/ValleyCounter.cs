189d412c696b8f50be3ef30712c936b559311253
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:32:57 +0100

    Counting Valleys

---
+++ src/Exercise/H4x0rR4nk/CountingValleys/ValleyCounter.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise.H4x0rR4nk.CountingValleys
+{
+    // https://www.hackerrank.com/challenges/counting-valleys/problem
+    public class ValleyCounter
+    {
+        static int countingValleys(int n, string s)
+        {
+            var valleys = 0;
+            var level = 0;
+            foreach (var direction in s)
+            {
+                if (direction == 'U')
+                {
+                    ++level;
+                }
+                else
+                {
+                    --level;
+                }
+
+                if (direction == 'U' && level == 0)
+                {
+                    ++valleys;
+                }
+            }
+
+            return valleys;
+        }
+    }
+}

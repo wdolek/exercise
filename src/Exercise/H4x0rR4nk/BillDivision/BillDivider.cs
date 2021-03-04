f6777a8e7e0666531019049812aaeea140c8b268
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:32:03 +0100

    Bill Division

---
+++ src/Exercise/H4x0rR4nk/BillDivision/BillDivider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise.H4x0rR4nk.BillDivision
+{
+    // https://www.hackerrank.com/challenges/bon-appetit/problem
+    public class BillDivider
+    {
+        static void bonAppetit(List<int> bill, int k, int b)
+        {
+            var annasBill = bill.Sum() - bill[k];
+            var split = annasBill / 2;
+
+            if (split == b)
+            {
+                Console.WriteLine("Bon Appetit");
+            }
+            else
+            {
+                Console.WriteLine(b - split);
+            }
+        }
+    }
+}

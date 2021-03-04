56874d1ca14db3a41b3f7460dc4d28949d16579c
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:35:45 +0100

    Staircase

---
+++ src/Exercise/H4x0rR4nk/Staircase/Staircasator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise.H4x0rR4nk.Staircase
+{
+    // https://www.hackerrank.com/challenges/staircase/problem
+    public class Staircasator
+    {
+        static void staircase(int n)
+        {
+            for (var i = 0; i < n; i++)
+            {
+                var level = new String('#', i + 1);
+                Console.WriteLine(level.PadLeft(n));
+            }
+        }
+    }
+}

3fddbf78bc283d7dee78279cc3145baef10eb584
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:34:45 +0100

    Mini-Max Sum

---
+++ src/Exercise/H4x0rR4nk/MiniMaxSum/MiniMaxSumator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise.H4x0rR4nk.MiniMaxSum
+{
+    // https://www.hackerrank.com/challenges/mini-max-sum/problem
+    public class MiniMaxSumator
+    {
+        static void miniMaxSum(int[] arr)
+        {
+            var ordered =
+                arr.OrderBy(i => i)
+                    .Select(i => (long)i)
+                    .ToArray();
+
+            Console.WriteLine(
+                "{0} {1}",
+                ordered.Take(4).Sum(),
+                ordered.Skip(1).Sum());
+        }
+    }
+}

0ac3f30a8b8b2eba8ea8367ed3c6d7f0944da8d8
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:34:58 +0100

    Number Line Jumper

---
+++ src/Exercise/H4x0rR4nk/NumberLineJumps/NumberLineJumper.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise.H4x0rR4nk.NumberLineJumps
+{
+    // https://www.hackerrank.com/challenges/kangaroo/problem
+    public class NumberLineJumper
+    {
+        static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            for (int jumps = 0, i1 = x1, i2 = x2; jumps < 10000; jumps++, i1 += v1, i2 += v2)
+            {
+                if (i1 == i2)
+                {
+                    return "YES";
+                }
+            }
+
+            return "NO";
+        }
+    }
+}

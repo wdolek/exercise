b645670b73f398a26e13b8a4e523823053197df3
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:36:15 +0100

    XOR Strings

---
+++ src/Exercise/H4x0rR4nk/XorStrings/StringXorator.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Exercise.H4x0rR4nk.XorStrings
+{
+    // https://www.hackerrank.com/challenges/strings-xor/problem
+    public class StringXorator
+    {
+        public static string strings_xor(string s, string t)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == t[i])
+                {
+                    sb.Append('0');
+                }
+                else
+                {
+                    sb.Append('1');
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}

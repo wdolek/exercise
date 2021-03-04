3f86993457f05c042a48b7e300ca24ea21265359
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:32:15 +0100

    Birthday Candles

---
+++ src/Exercise/H4x0rR4nk/BirthDayCandles/BirthDayCandlator.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise.H4x0rR4nk.BirthDayCandles
+{
+    // https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    public class BirthDayCandlator
+    {
+        static int birthdayCakeCandles(int[] ar)
+        {
+            var maxHeight = int.MinValue;
+            var counter = 0;
+
+            for (var i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] > maxHeight)
+                {
+                    maxHeight = ar[i];
+                    counter = 1;
+                }
+                else if (ar[i] == maxHeight)
+                {
+                    ++counter;
+                }
+            }
+
+            return counter;
+        }
+    }
+}

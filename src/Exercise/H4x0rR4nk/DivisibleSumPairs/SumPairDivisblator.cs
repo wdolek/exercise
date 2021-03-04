10e823869a03dbbe968ab2ac8cae7fac57530584
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:33:35 +0100

    Divisible Sum Pairs

---
+++ src/Exercise/H4x0rR4nk/DivisibleSumPairs/SumPairDivisblator.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise.H4x0rR4nk.DivisibleSumPairs
+{
+    // https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+    public class SumPairDivisblator
+    {
+        static int divisibleSumPairs(int n, int k, int[] ar)
+        {
+            var bucket = new int[k];
+            var count = 0;
+
+            foreach (var value in ar)
+            {
+                var modValue = value % k;
+                count += bucket[(k - modValue) % k];
+                bucket[modValue]++;
+            }
+
+            return count;
+        }
+    }
+}

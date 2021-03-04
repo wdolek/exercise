387e36e5bfac6fa4e8397d0fe9128c9aab8c044d
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:35:16 +0100

    Plus-Minus

---
+++ src/Exercise/H4x0rR4nk/PlusMinus/PlusMinusator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise.H4x0rR4nk.PlusMinus
+{
+    // https://www.hackerrank.com/challenges/plus-minus/problem
+    public class PlusMinusator
+    {
+        static void plusMinus(int[] arr)
+        {
+            var positiveNumbers = arr.Count(i => i > 0);
+            Console.WriteLine("{0:0.000000}", (float)positiveNumbers / arr.Length);
+
+            var negativeNumbers = arr.Count(i => i < 0);
+            Console.WriteLine("{0:0.000000}", (float)negativeNumbers / arr.Length);
+
+            var zeros = arr.Count(i => i == 0);
+            Console.WriteLine("{0:0.000000}", (float)zeros / arr.Length);
+        }
+    }
+}

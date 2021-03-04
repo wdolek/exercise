4c0586af9311e1390a9b4e93cd04d81f9ab644db
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:33:56 +0100

    Extra Long Factorial

---
+++ src/Exercise/H4x0rR4nk/ExtraLongFactorials/ExtraLongFactorialCalculon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace Exercise.H4x0rR4nk.ExtraLongFactorials
+{
+    // https://www.hackerrank.com/challenges/extra-long-factorials/problem
+    public class ExtraLongFactorialCalculon
+    {
+        static void extraLongFactorials(int n)
+        {
+            var result = new BigInteger(n);
+            for (var i = n - 1; i > 0; i--)
+            {
+                result = BigInteger.Multiply(result, new BigInteger(i));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

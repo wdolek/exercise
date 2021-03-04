634c22beb3a8523689aa518862b10a6334f64ee3
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:35:36 +0100

    Simple Array Sum

---
+++ src/Exercise/H4x0rR4nk/SimpleArraySum/SimpleArraySumator.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Exercise.H4x0rR4nk.SimpleArraySum
+{
+    // https://www.hackerrank.com/challenges/simple-array-sum/problem
+    public class SimpleArraySumator
+    {
+        static int simpleArraySum(int[] ar)
+        {
+            return ar.Sum();
+        }
+
+        static int simpleArraySumLinqLess(int[] ar)
+        {
+            var sum = 0;
+            for (var i = ar.Length - 1; i >= 0; --i)
+            {
+                sum += ar[i];
+            }
+
+            return sum;
+        }
+    }
+}

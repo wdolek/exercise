063f35c2febd4c1f5313e7f6bba5bc7e3835465e
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:35:25 +0100

    Sales by Match

---
+++ src/Exercise/H4x0rR4nk/SalesByMatch/SockSeller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise.H4x0rR4nk.SalesByMatch
+{
+    // https://www.hackerrank.com/challenges/sock-merchant/problem
+    public class SockSeller
+    {
+        static int sockMerchant(int n, int[] ar)
+        {
+            var sockCounts = new Dictionary<int, int>(n);
+            for (var i = 0; i < ar.Length; i++)
+            {
+                var sockColor = ar[i];
+                if (!sockCounts.ContainsKey(sockColor))
+                {
+                    sockCounts[sockColor] = 0;
+                }
+                ++sockCounts[sockColor];
+            }
+
+            var pairs = 0;
+            foreach (var kvp in sockCounts)
+            {
+                pairs += kvp.Value / 2;
+            }
+
+            return pairs;
+        }
+    }
+}

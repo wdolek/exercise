91572a313a34289718947c5b801cf9befe1d483f
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:34:24 +0100

    Grading Students

---
+++ src/Exercise/H4x0rR4nk/GradingStudents/StudentsGrader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise.H4x0rR4nk.GradingStudents
+{
+    // https://www.hackerrank.com/challenges/grading/problem
+    public class StudentsGrader
+    {
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            var result = new List<int>();
+            foreach (var grade in grades)
+            {
+                if (grade < 38)
+                {
+                    result.Add(grade);
+                    continue;
+                }
+
+                var roundUp = ((grade / 5) + 1) * 5;
+                if (roundUp - grade < 3)
+                {
+                    result.Add(roundUp);
+                }
+                else
+                {
+                    result.Add(grade);
+                }
+            }
+
+            return result;
+        }
+    }
+}

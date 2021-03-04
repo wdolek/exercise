01a7dc1ddfa7875b8fd9caa1949c740b417e6d73
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:34:35 +0100

    Migratory Birds

---
+++ src/Exercise/H4x0rR4nk/MigratoryBirds/BirdMigrator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise.H4x0rR4nk.MigratoryBirds
+{
+    // https://www.hackerrank.com/challenges/migratory-birds/problem
+    public class BirdMigrator
+    {
+        static int migratoryBirds(List<int> arr)
+        {
+            var c = new int[5];
+            for (var i = 0; i < arr.Count; i++)
+            {
+                ++c[arr[i] - 1];
+            }
+
+            return c.Select((v, i) => new { TypeId = i + 1, Count = v })
+                .OrderByDescending(t => t.Count)
+                .ThenBy(t => t.TypeId)
+                .First()
+                .TypeId;
+        }
+    }
+}

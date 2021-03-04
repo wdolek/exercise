3215c3e718daad9968fda24495cc4b4ac0cb2e48
Zdenek Havlin <wdolek@hotmail.com>
2021-03-04 21:36:04 +0100

    Very Big Sum

---
+++ src/Exercise/H4x0rR4nk/VeryBigSum/VeryBigSumator.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise.H4x0rR4nk.VeryBigSum
+{
+    // https://www.hackerrank.com/challenges/a-very-big-sum/submissions
+    public class VeryBigSumator
+    {
+        static long aVeryBigSum(long[] ar)
+        {
+            var result = 0L;
+
+            for (var i = 0; i < ar.Length; i++)
+            {
+                unchecked
+                {
+                    result += ar[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}

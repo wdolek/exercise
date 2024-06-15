9917df4b2869fcf475a4bbf4d1cba3672906794c
Zdenek Havlin <wdolek@hotmail.com>
2024-06-15 21:26:35 +0200

    Add space arrange problem

---
+++ src/Exercise/L33tC0d3/RearrangeSpacesBetweenWords/SpaceArranger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Exercise.L33tC0d3.RearrangeSpacesBetweenWords;
+
+// https://leetcode.com/problems/rearrange-spaces-between-words (1592)
+public class SpaceArranger
+{
+    public string ReorderSpaces(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return text;
+        }
+
+        var numOfSpaces = text.Count(c => c == ' ');
+        var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        // formula:
+        // - number of spaces between words = <number of spaces> / <number of words - 1>
+        // - trailing space(s) = <number of spaces> % <number of words - 1>
+        var numOfSpacesBetweenWords = Math.DivRem(numOfSpaces, words.Length - 1, out var numOfTrailingSpaces);
+
+        // 💡 alternatively `StringBuilder.AppendJoin` could be used
+        var sb = new StringBuilder(text.Length);
+
+        // append first word separately, then keep appending by starting with spaces
+        // (this is to avoid if-condition checking whether to not append spaces after last word)
+        sb.Append(words[0]);
+        for (var i = 1; i < words.Length; i++)
+        {
+            sb.Append(' ', numOfSpacesBetweenWords);
+            sb.Append(words[i]);
+        }
+
+        sb.Append(' ', numOfTrailingSpaces);
+
+        return sb.ToString();
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/RearrangeSpacesBetweenWords/SpaceArrangerShould.cs
@@ -0,0 +1,29 @@
+﻿using Exercise.L33tC0d3.RearrangeSpacesBetweenWords;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.RearrangeSpacesBetweenWords;
+
+public class SpaceArrangerShould
+{
+    [Fact]
+    public void RearrangeSpacesWithoutTrailingSpace()
+    {
+        // act
+        var arranger = new SpaceArranger();
+        var result = arranger.ReorderSpaces("  this   is  a sentence ");
+
+        // assert
+        Assert.Equal("this   is   a   sentence", result);
+    }
+
+    [Fact]
+    public void RearrangeSpacesWithTrailingSpace()
+    {
+        // act
+        var arranger = new SpaceArranger();
+        var result = arranger.ReorderSpaces(" practice   makes   perfect");
+
+        // assert
+        Assert.Equal("practice   makes   perfect ", result);
+    }
+}

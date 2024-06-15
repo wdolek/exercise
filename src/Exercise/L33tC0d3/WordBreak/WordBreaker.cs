9d145228809d847de2969aba140e6c8a7ce79e4a
Zdenek Havlin <wdolek@hotmail.com>
2024-06-15 21:48:11 +0200

    Add word-break problem; Use listed solution (dynamic programming (⊙x⊙;)

---
+++ src/Exercise/L33tC0d3/WordBreak/WordBreaker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Exercise.L33tC0d3.WordBreak;
+
+// https://leetcode.com/problems/word-break/ (139)
+// solution: https://leetcode.com/problems/word-break/solutions/3834571/c-easy-to-understand-solution-beats-runtime-99-memory-28
+public class WordBreaker
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        // dynamic programming; dp[i] = true if s[i..] can be broken into words in wordDict
+        Span<bool> dp = stackalloc bool[s.Length + 1];
+        dp[^1] = true;
+
+        var inputSpan = s.AsSpan();
+
+        // pass reference to the List's internal memory to speed up iteration (using marshalling),
+        // or fallback to `.GetEnumerator()` and `.MoveNext()` if `wordDict` is not a List<T>
+        // ---
+        // NB! this is a micro-optimization; will perhaps speed up iteration, we still need to access heap for string value itself;
+        //     benchmark to see if it's worth it ¯\_(ツ)_/¯
+        if (wordDict is List<string> wordDictList)
+        {
+            var wordDictSpan = CollectionsMarshal.AsSpan(wordDictList);
+            return WordBreakSpanImpl(ref dp, inputSpan, wordDictSpan);
+        }
+
+        return WordBreakEnumerableImpl(ref dp, inputSpan, wordDict);
+    }
+
+    private static bool WordBreakEnumerableImpl(ref Span<bool> dp, in ReadOnlySpan<char> s, IList<string> wordDict)
+    {
+        // iterate from the end of the string...
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            // ... and try to match the word from provided dictionary
+            foreach (var word in wordDict)
+            {
+                // current word won't fit into the remaining string, skip
+                if (i + word.Length > s.Length)
+                {
+                    continue;
+                }
+
+                // current word matches, mark the current position and continue further
+                var slice = s.Slice(i, word.Length);
+                if (slice.Equals(word, StringComparison.Ordinal))
+                {
+                    dp[i] = dp[i + word.Length];
+                    if (dp[i])
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    private static bool WordBreakSpanImpl(ref Span<bool> dp, in ReadOnlySpan<char> s, in Span<string> wordDict)
+    {
+        // iterate from the end of the string...
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            // ... and try to match the word from provided dictionary
+            foreach (var word in wordDict)
+            {
+                // current word won't fit into the remaining string, skip
+                if (i + word.Length > s.Length)
+                {
+                    continue;
+                }
+
+                // current word matches, mark the current position and continue further
+                var slice = s.Slice(i, word.Length);
+                if (slice.Equals(word, StringComparison.Ordinal))
+                {
+                    dp[i] = dp[i + word.Length];
+                    if (dp[i])
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+    }
+}
---
+++ tests/Exercise.Tests/L33tC0d3/WordBreak/WordBreakerShould.cs
@@ -0,0 +1,34 @@
+﻿using Exercise.L33tC0d3.WordBreak;
+using Xunit;
+
+namespace Exercise.Tests.L33tC0d3.WordBreak;
+
+public class WordBreakerShould
+{
+    [Fact]
+    public void EvaluateNonRepeatingStrings()
+    {
+        var breaker = new WordBreaker();
+        var canBeSegmented = breaker.WordBreak("leetcode", ["leet", "code"]);
+
+        Assert.True(canBeSegmented);
+    }
+
+    [Fact]
+    public void EvaluateRepeatingStrings()
+    {
+        var breaker = new WordBreaker();
+        var canBeSegmented = breaker.WordBreak("applepenapple", ["apple", "pen"]);
+
+        Assert.True(canBeSegmented);
+    }
+
+    [Fact]
+    public void IndicateNoSegmentation()
+    {
+        var breaker = new WordBreaker();
+        var canBeSegmented = breaker.WordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]);
+
+        Assert.False(canBeSegmented);
+    }
+}
